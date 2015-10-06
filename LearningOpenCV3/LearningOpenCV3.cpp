c5b3085d6dde013642852bba7ce33aa1a0654b86
william-z <alan_zhou@qq.com>
2015-10-06 12:35:49 +0800

    canny edges

--- LearningOpenCV3/LearningOpenCV3.cpp
+++ LearningOpenCV3/LearningOpenCV3.cpp
@@ -3,9 +3,53 @@
 
 #include "stdafx.h"
 
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
-int _tmain(int argc, _TCHAR* argv[])
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+#define forever for(;;)
+
+int main(int argc, char** argv)
 {
+	if (argc < 2) return -1;
+
+	if (argc == 2) {
+		if (strcmp(argv[1], "edges") == 0) {
+			VideoCapture cap(0);
+			if (!cap.isOpened()) return -1;
+
+			Mat frame, edges;
+			namedWindow("edges");
+			forever{
+				cap >> frame;
+				cvtColor(frame, edges, CV_BGR2GRAY);
+				GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
+				Canny(edges, edges, 0, 30, 3);
+				imshow("edges", edges);
+				if (waitKey(30) >= 0) break;
+			}
+
+			return 0;
+		}
+	}
+
+	if (argc == 7) {
+		Mat src = imread(argv[1]);
+		int x = atoi(argv[2]);
+		int y = atoi(argv[3]);
+		int width = atoi(argv[4]);
+		int height = atoi(argv[5]);
+		int add = atoi(argv[6]);
+
+		Rect roi(x, y, width, height);
+		
+	}
+
 	return 0;
 }
+
 
